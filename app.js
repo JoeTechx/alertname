a636ac127de46f496aa88e7e0ac5654a8e8aa0b5
joe tech <ojtech2003@gmail.com>
2022-06-24 04:56:03 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+// const fullName = prompt("FullName: ");
+// const Height = prompt("Height: ");
+// const Country = prompt("Country: ")
+
+// document.write(`${fullName} <br> ${Height} <br> ${Country}`);
+
+
+const aboutMe = {
+    name: 'Oyinloye Joseph',
+    height: '5.3ft',
+    country: 'Nigeria'
+}
+
+alert(`I am ${aboutMe.name}
+I am ${aboutMe.height} tall
+I am ffrom${aboutMe.country}`)
+
+
